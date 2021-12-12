cd7074f08ec1c29e1467c3faeb7614c7004bf0cb
NichoFerez <54872650+NichoFerez@users.noreply.github.com>
2021-12-12 01:48:10 -0500

    dsaf
    dsaf

--- Map.js
+++
@@ -1,9 +0,0 @@
-	mapboxgl.accessToken = 'pk.eyJ1IjoibmljaG9mZXJleiIsImEiOiJja2R6MGozYjkyZmpoMzBscTBweHg1dWRjIn0.0hLtqRVLovWKojOT_EWixQ';
-
-
-var map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/nichoferez/cki5awdxb2guv1apc7mlbjsyc', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
